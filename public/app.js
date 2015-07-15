fc222a5097b00c7ec4c22808e0da91fe200b286a
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-07-15 18:48:20 +0100

    Add basic interface functionality to click on answers and increment score

--- public/app.js
+++ public/app.js
@@ -1,1 +1,43 @@
-app.js
+var game = new Game();
+
+$("#answer1a").on('click', function() {
+  event.preventDefault();
+  game.checkAnswer('a');
+  $('#result').html(game.score);
+});
+
+$("#answer1b").on('click', function() {
+  event.preventDefault();
+});
+
+$("#answer1c").on('click', function() {
+  event.preventDefault();
+});
+
+$("#answer2b").on('click', function() {
+  event.preventDefault();
+  game.checkAnswer('b');
+  $('#result').html(game.score);
+});
+
+$("#answer1a").on('click', function() {
+  event.preventDefault();
+});
+
+$("#answer1c").on('click', function() {
+  event.preventDefault();
+});
+
+$("#answer3c").on('click', function() {
+  event.preventDefault();
+  game.checkAnswer('c');
+  $('#result').html(game.score);
+});
+
+$("#answer3a").on('click', function() {
+  event.preventDefault();
+});
+
+$("#answer1b").on('click', function() {
+  event.preventDefault();
+});
--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function(req, res){
   res.sendFile('index.html');
 });
 
+// app.get('/quiz', function(req, res){
+//   res.sendFile('quiz.html');
+// });
+
 server.listen(3000, function(){
   console.log("Server listening on port 3000");
 });
