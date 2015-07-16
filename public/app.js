0e85985f6b93afe787fe8b54d7781e0e52c7b4ad
Natalie Akam <natalie.akam@gmail.com>
2015-07-16 11:51:52 +0100

    Updated backend logic and front end display logic

--- public/app.js
+++ public/app.js
@@ -1,1 +1,42 @@
-app.js
+$(document).ready(function() {
+
+  var game = new Game;
+  game.setGame();
+  showEverything();
+
+  function showEverything() {
+  	if (game.playing === false) {
+      $('#quiz-main').hide();
+      $('#score-main').show();
+      $('#final').html(game.score);
+      $('#outof').html(game.noQuestions);
+    }
+    $("#turn").html(game.turn + 1);
+    $("#score").html(game.score);
+    $("#noQuestions").html(game.noQuestions);
+    $('#question').html(game.questions[game.turn]);
+    $('#a').html(game.answerAs[game.turn]);
+    $('#b').html(game.answerBs[game.turn]);
+    $('#c').html(game.answerCs[game.turn]);
+    $("#playing").html(game.playing);
+  }
+
+  $("#a").click(function() {
+    game.checkAnswer('a');
+    showEverything();
+  });
+
+  $("#b").click(function() {
+    game.checkAnswer('b');
+    showEverything();
+  });
+
+  $("#c").click(function() {
+    game.checkAnswer('c');
+    showEverything();
+  }); 
+
+  $("#replay").click(function() {
+    location.reload();
+  }); 
+});
